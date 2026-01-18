215354ed09ec6292756d5e2d4dc36dacfe40436f
EldarToleubay <49515746+EldarToleubay@users.noreply.github.com>
2026-01-18 06:09:28 +0500

    cross

--- src/main/java/com/pairapp/controller/MoodRequestController.java
+++ src/main/java/com/pairapp/controller/MoodRequestController.java
@@ -7,18 +7,14 @@ import com.pairapp.security.SecurityUtils;
 import com.pairapp.service.MoodRequestService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/mood-requests")
+@CrossOrigin("*")
 public class MoodRequestController {
     private final MoodRequestService moodRequestService;
 
